6dbc6e3d6cf2071247825ab697200af293f46975
biagemc <biagemc@gmail.com>
2024-12-11 09:04:15 -0700

    Add additional retry strategies for business admin, guest, and system activities

--- lib/sidekiq_retry_strategy.rb
+++ lib/sidekiq_retry_strategy.rb
@@ -5,6 +5,9 @@ require "sidekiq_retry_strategy/configuration"
 require "sidekiq_retry_strategy/retry_logic_helpers"
 require "sidekiq_retry_strategy/custom_retry_logic"
 require "sidekiq_retry_strategy/strategies/default_retry"
+require "sidekiq_retry_strategy/strategies/business_admin_activity_retry"
+require "sidekiq_retry_strategy/strategies/guest_activity_retry"
+require "sidekiq_retry_strategy/strategies/system_activity_retry"
 
 module SidekiqRetryStrategy
   # Config loads from initializer in the Rails app, using ENV overrides.
