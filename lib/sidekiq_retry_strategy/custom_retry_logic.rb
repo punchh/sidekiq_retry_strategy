af2abaf5b2b4d93adda8751c6db86648322343ae
biagemc <biagemc@gmail.com>
2025-01-13 21:30:36 -0700

    Update README and retry logic to enhance customization options for Sidekiq retries

--- lib/sidekiq_retry_strategy/custom_retry_logic.rb
+++ lib/sidekiq_retry_strategy/custom_retry_logic.rb
@@ -9,14 +9,15 @@ module SidekiqRetryStrategy
 
     included do
       sidekiq_retry_in do |count, exception, jobhash|
-        retry_params = self.retry_params
+        set_params_override = self.is_params_overridable(sidekiq_options)
+        retry_params = set_params_override.present? ? set_params_override : self.retry_params.transform_keys(&:to_sym)
 
-        if retry_params.nil? || retry_params['max_retries'].nil? || retry_params['delays'].nil?
+        if retry_params.nil? || retry_params[:max_retries].nil? || retry_params[:delays].nil?
           Sidekiq.logger.warn("Invalid retry_params for #{jobhash['class']} with args #{jobhash['args']}")
           :discard
         else
-          max_retries = retry_params['max_retries']
-          delays = retry_params['delays'].size != max_retries ? self.expand_delays_array(retry_params['delays'], max_retries) : retry_params['delays']
+          max_retries = retry_params[:max_retries]
+          delays = retry_params[:delays].size != max_retries ? self.expand_delays_array(retry_params[:delays], max_retries) : retry_params[:delays]
 
           if count >= max_retries
             sidekiq_retries_exhausted_block.call(jobhash, exception)
@@ -44,8 +45,20 @@ module SidekiqRetryStrategy
       end
     end
 
-    # Define class methods that are overridable by the worker class
     class_methods do
+      def is_params_overridable(options)
+        # Override retry options in the worker class to set custom retry options in the sidekiq_options hash
+        # Expected value to set - `set_retry_options: {delays: [100, 200], max_retries: 2}`
+        # Example: `sidekiq_options queue: :campaigns, retry: true, set_retry_options: {delays: [100, 200], max_retries: 2}`
+        # This will override the retry_params in the worker class
+        # Checking if the sidekiq value for `set_retry_options` is defined
+        if options.respond_to?(:key?) && options.key?('set_retry_options')
+          options['set_retry_options']
+        else
+          nil
+        end
+      end
+    
       def is_kill_on_exception(exception, jobhash, count)
         if method_defined_in_worker?(:is_kill_on_exception)
           self.is_kill_on_exception(exception, jobhash, count)
